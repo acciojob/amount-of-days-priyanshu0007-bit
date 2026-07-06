e66185b015a43b40dce520ffa497a349fe6e182a
priyanshu ahir <priyanshuahir777@gmail.com>
2026-07-06 22:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfyear(year) {
+	if(year%400==0){
+		366;
+	}else if(year%100==0){
+		return 365;
+	}else if(year%4==0){
+		return 365;
+	}else{
+		return 366;
+	}
+}
